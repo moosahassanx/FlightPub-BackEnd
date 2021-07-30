46c28e08d301bbd48b903b85e9e20b4ee00696f7
AAlkazraji <82151169+AAlkazraji@users.noreply.github.com>
2021-07-30 23:08:43 +1000

    api for booking

--- src/main/java/com/seng3150/flightpub/repository/AvailabilityRepository.java
+++ src/main/java/com/seng3150/flightpub/repository/AvailabilityRepository.java
@@ -1,9 +1,23 @@
 package com.seng3150.flightpub.repository;
 
 import com.seng3150.flightpub.models.Availability;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Date;
+import java.util.List;
 
 public interface AvailabilityRepository extends JpaRepository<Availability, String>, JpaSpecificationExecutor<Availability> {
+
+    @Query(value = "SELECT * " +
+            "FROM Availability " +
+            "WHERE Availability.flight_number = ?2 " +
+            "AND datediff(day, Availability.departure_time, ?1) = 0 " +
+            "AND Availability.number_available_seats_leg1 > ?3 " +
+            "AND Availability.number_available_seats_leg2 > ?3", nativeQuery = true)
+     List<Availability> findFlightsAvailability(Date depTime , String flightNumber, int number_available_seats_leg1);
+
 
 }
