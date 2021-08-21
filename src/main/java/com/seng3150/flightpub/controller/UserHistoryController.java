61cb7d10318370a0ad68db99490288fddd0b78cd
S I <C3181999@uon.edu.au>
2021-08-21 14:31:36 +1000

    removed redundant recommendations code.

--- src/main/java/com/seng3150/flightpub/controller/UserHistoryController.java
+++ src/main/java/com/seng3150/flightpub/controller/UserHistoryController.java
@@ -1,8 +1,6 @@
 package com.seng3150.flightpub.controller;
 
 import com.seng3150.flightpub.models.Booking;
-import com.seng3150.flightpub.models.Destinations;
-import com.seng3150.flightpub.repository.DestinationsRepository;
 import com.seng3150.flightpub.repository.UserHistoryRepository;
 import com.seng3150.flightpub.repository.UserRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +14,11 @@ public class UserHistoryController
 {
     private final UserHistoryRepository userHistoryRepository;
     private final UserRepository userRepository;
-    private final DestinationsRepository destinationsRepository;
 
-    public UserHistoryController(UserHistoryRepository userHistoryRepository, UserRepository userRepository, DestinationsRepository destinationsRepository)
+    public UserHistoryController(UserHistoryRepository userHistoryRepository, UserRepository userRepository)
     {
         this.userHistoryRepository = userHistoryRepository;
         this.userRepository = userRepository;
-        this.destinationsRepository = destinationsRepository;
     }
 
     @RequestMapping("/getUserHistory")
@@ -30,11 +26,5 @@ public class UserHistoryController
     {
         long userId = userRepository.getDetailsByUserName(userName).getId();
         return userHistoryRepository.getUserFlightHistory((int)userId);
-    }
-
-    @RequestMapping("/getTop3Destinations")
-    List<Destinations> getTop3Destinations()
-    {
-        return destinationsRepository.getTop3Destinations();
     }
 }
