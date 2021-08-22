3b29ad7e52db279108fe85c747b3fe85062ac9d6
Moosa <moosa7021@gmail.com>
2021-08-22 12:29:48 +1000

    trying to deploy to heroku app

--- src/main/resources/UpdateDates.sql
+++ src/main/resources/UpdateDates.sql
@@ -1,18 +1,18 @@
   USE FlightPub;
 
 UPDATE Availability
-SET departure_time = DATEADD(YEAR, 1, departure_time)
+SET departure_time = DATEADD(YEAR, 6, departure_time)
 
 UPDATE Price
-SET start_date=DATEADD(YEAR, 1, start_date),
-    end_date=DATEADD(YEAR, 1, end_date)
+SET start_date=DATEADD(YEAR, 6, start_date),
+    end_date=DATEADD(YEAR, 6, end_date)
 
 UPDATE Flights
-SET departure_time=DATEADD(YEAR, 1, departure_time),
-    arrival_time=DATEADD(YEAR, 1, arrival_time)
+SET departure_time=DATEADD(YEAR, 6, departure_time),
+    arrival_time=DATEADD(YEAR, 6, arrival_time)
 
 
 UPDATE Flights
-SET arrival_time_stop_over=DATEADD(YEAR, 1, arrival_time_stop_over),
-    departure_time_stop_over=DATEADD(YEAR, 1, departure_time_stop_over)
+SET arrival_time_stop_over=DATEADD(YEAR, 6, arrival_time_stop_over),
+    departure_time_stop_over=DATEADD(YEAR, 6, departure_time_stop_over)
 WHERE arrival_time_stop_over IS NOT NULL;
